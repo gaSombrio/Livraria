aa8bb64fe2390914202ac48e14e33ebca2b7078c
gaSombrio <sombrio.ga@gmail.com>
2026-02-24 14:48:01 -0300

    feat: criação da API para Editora

---
+++ core/serializers/editora.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from core.models import Editora
+
+
+class EditoraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
